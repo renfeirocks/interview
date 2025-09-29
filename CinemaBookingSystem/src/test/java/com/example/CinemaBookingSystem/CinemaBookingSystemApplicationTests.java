2be6f08b5bf478d054fa83969b38fe337296570a
Renfei <wangrenfei1991@gmail.com>
2025-09-29 19:36:53 +0800

    initialize CinemaBookingSystem project
    initialize CinemaBookingSystem project with Spring Boot setup

---
+++ CinemaBookingSystem/src/test/java/com/example/CinemaBookingSystem/CinemaBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
